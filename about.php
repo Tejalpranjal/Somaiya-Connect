751e547c1d648e6a3df459af2d77e446e6f6519e
Tejalpranjal <105512640+Tejalpranjal@users.noreply.github.com>
2023-04-22 20:12:15 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+  <title>Welcome to Somaiya Connect forums</title>
+  <style>
+    section{
+      height: 70vh;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include 'partial/_header.php'; ?>
+
+  <main role="main">
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+    <img src="img6.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption text-left">
+          <h1>Example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+    <div class="carousel-item">
+    <img src="img8.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption">
+          <h1>Another example headline.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+    <div class="carousel-item active">
+    <img src="img7.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
+      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
+      <!-- <div class="container">
+        <div class="carousel-caption text-right">
+          <h1>One more for good measure.</h1>
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+        </div>
+      </div> -->
+      <div class="carousel-caption d-none d-md-block">
+                    <!-- <h1>Welcome</h1> -->
+                    <h1>Our Team</h1>
+          </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+
+<div class="container marketing bg-light">
+<hr class="featurette-divider">
+<h1 class="text-center my-3">Our Mission</h1>
+  <div class="row featurette">
+
+    <div class="col-md-7">
+      <h2 class="featurette-heading">First featurette. <span class="text-muted">It’ll blow your mind.</span></h2>
+      <p class="lead">Somaiya Connect mission is to share and grow the knowledge. A vast amount of the knowledge that would be valuable to many people is currently only available to a few categories. We want to connect the people who have knowledge to the people who need it.</p>
+    </div>
+    <div class="col-md-5">
+    <img src="img9.jpg" alt="" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" >
+
+      <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+    </div>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7 order-md-2">
+      <h2 class="featurette-heading">Oh yeah, it is our solution. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">It is an initiative to help the upcoming students with the discussion forum.Somaiya Connect focuses on providing the most efficient way to solve your queries or solution wants to be simple way.We will help students build up concepts and easy to solve their queries.</p>
+    </div>
+    <div class="col-md-5 order-md-1">
+    <img src="img10.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" alt="" width="500" height="500">
+    </div>
+  </div>
+
+  <hr class="featurette-divider">
+
+  <div class="row featurette">
+    <div class="col-md-7">
+      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+      <p class="lead">There are number of students who has better knowledge, our motive is connect all the students to this discussion forum it will help you to solve your ideas and queries easily. </p>
+    </div>
+    <div class="col-md-5">
+    <img src="img11.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" alt="" width="500" height="500">
+
+    </div>
+  </div>
+
+
+  <!-- Three columns of text below the carousel -->
+  <div class="container my-5">
+  <div class="text-center">
+    <hr class="w-100 mx-auto">
+    <h1 class="display-4"><b>About Us</b></h1>
+    <hr class="w-100 mx-auto">
+  </div>
+</div>
+
+<div class="container my-5">
+  <section class="ourteam text-center">
+    <div class="container">
+      <h1>OUR AMAZING TEAM</h1>
+      <p>Who are we?</p>
+      <div class="row teamsetting">
+        <div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+          <figure class="figure">
+            <img src="tejal.png" class="figure-img img-fluid rounded-circle" height="200px" width="200px">
+            <figcaption>
+              <h4>Tejal Ambhore </h4>
+              <p class="figure-caption"> Student</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+          <figure class="figure">
+            <img src="jiya.png" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+            <figcaption>
+              <h4>Jiya Bhagat </h4>
+              <p class="figure-caption"> Student</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+          <figure class="figure">
+            <img src="nidhi.png" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+            <figcaption>
+              <h4>Nidhi Bhandari </h4>
+              <p class="figure-caption"> Student</p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+  <!-- START THE FEATURETTES -->
+
+ 
+  <hr class="featurette-divider">
+
+  <!-- /END THE FEATURETTES -->
+
+</div><!-- /.container -->
+
+
+<!-- FOOTER -->
+<!-- <footer class="container">
+  <p class="float-right"><a href="#">Back to top</a></p>
+  <p>© 2017-2020 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+</footer> -->
+</main>
+     <?php include 'partial/footer.php';  ?>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
