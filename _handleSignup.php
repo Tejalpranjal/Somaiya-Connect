1747ce46dba3328d2cd1ddaa43680ab7462feb12
Nidhi703 <105424777+Nidhi703@users.noreply.github.com>
2023-04-27 14:05:04 +0530

    added different errors

--- _handleSignup.php
+++ _handleSignup.php
@@ -3,7 +3,7 @@ $showError="false";
 $showAlert=false;
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     include 'db_connect.php';
-    $user_email=$_POST['signupEmail'];
+    $useremail=$_POST['signupEmail'];
     $user=str_replace("<","&lt;","$user");
     $user=str_replace(">","&gt;","$user");
     $user=str_replace('"',"&quot;","$user");
@@ -22,36 +22,42 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $username = str_replace("'", "&apos;", $username);
     
     $cpass=$_POST['signupcPassword'];
+    // Get the user's email ID from the input form
 
-
-    //cheak whether this email exists
-    $existsql="select * from `users` where user_email='$user_email'";
-    $result=mysqli_query($conn,$existsql);
-    $numRows=mysqli_num_rows($result);
-    if($numRows>0){
-        $showError="Email already in use";
-        header("Location: /collage_forum/index.php?signupsuccess=false&emailused=true");
+    // Check if the email ID is of the form "@somaiya.edu"
+    if (!preg_match('/^[a-zA-Z0-9._%+-]+@somaiya\.edu$/', $useremail)) {
+        // Redirect the user to an error page
+        $showError="Invalid email id!";
+        header("Location: /collage_forum/index.php?signupsuccess=false&error=true");
     }
-    else{
-      if($pass==$cpass){
-       $hash=password_hash($pass,PASSWORD_DEFAULT);
-       $sql="INSERT INTO `users` (`user_email`,`user_name`, `user_pass`, `timestamp`) VALUES ('$user_email','$username', '$hash', current_timestamp())";
-       $result=mysqli_query($conn,$sql);
-       if($result==true){
-           $showAlert=true;
-           echo $showAlert;
-           header("Location: /collage_forum/index.php?signupsuccess=true&error=false");
-
-       }
-      }
-      else{
-          $showError="passwords do not match";
-          header("Location: /collage_forum/index.php?signupsuccess=false&error=true");
-       }
-
-      }
-
-
+    
+    else {
+        //check whether this email exists
+        $existsql="select * from `users` where user_email='$useremail'";
+        $result=mysqli_query($conn,$existsql);
+        $numRows=mysqli_num_rows($result);
+        if($numRows>0){
+            $showError="Email already in use";
+            header("Location: /collage_forum/index.php?signupsuccess=false&emailused=true");
+        }
+        else{
+            if($pass==$cpass){
+                $hash=password_hash($pass,PASSWORD_DEFAULT);
+                $sql="INSERT INTO `users` (`user_email`,`user_name`, `user_pass`, `timestamp`) VALUES ('$useremail','$username', '$hash', current_timestamp())";
+                $result=mysqli_query($conn,$sql);
+                if($result==true){
+                    $showAlert=true;
+                    echo $showAlert;
+                    header("Location: /collage_forum/index.php?signupsuccess=true&error=false");
+                }
+            }
+            else{
+                $showError="Password do not match";
+                header("Location: /collage_forum/index.php?signupsuccess=false&passerror=true");
+            }
+        }
     }
+    
+}
     
 ?>
