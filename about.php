bfe13294bbf11a32268c949d8e0512096b4ba96b
Nidhi703 <105424777+Nidhi703@users.noreply.github.com>
2023-05-01 11:16:31 +0530

    Update about.php

--- about.php
+++ about.php
@@ -33,14 +33,7 @@
   <div class="carousel-inner">
     <div class="carousel-item">
     <img src="img6.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
-      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
-      <!-- <div class="container">
-        <div class="carousel-caption text-left">
-          <h1>Example headline.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-        </div>
-      </div> -->
+      
       <div class="carousel-caption d-none d-md-block">
                     <!-- <h1>Welcome</h1> -->
                     <h1>Our Team</h1>
@@ -48,14 +41,7 @@
     </div>
     <div class="carousel-item">
     <img src="img8.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
-      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
-      <!-- <div class="container">
-        <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-        </div>
-      </div> -->
+     
       <div class="carousel-caption d-none d-md-block">
                     <!-- <h1>Welcome</h1> -->
                     <h1>Our Team</h1>
@@ -63,14 +49,7 @@
     </div>
     <div class="carousel-item active">
     <img src="img7.jpg" alt="" class="bd-placeholder-img" width="100%" height="100%">
-      <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg> -->
-      <!-- <div class="container">
-        <div class="carousel-caption text-right">
-          <h1>One more for good measure.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-        </div>
-      </div> -->
+      
       <div class="carousel-caption d-none d-md-block">
                     <!-- <h1>Welcome</h1> -->
                     <h1>Our Team</h1>
