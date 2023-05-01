7e5814b3236f86433189c9e1bcb56a31b39e6827
Nidhi703 <105424777+Nidhi703@users.noreply.github.com>
2023-05-01 12:10:37 +0530

    Update _signupmodal.php

--- _signupmodal.php
+++ _signupmodal.php
@@ -12,7 +12,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="signupModalLabel">SignUp to Somaiya Connect</h5>
+                <h5 class="modal-title" id="signupModalLabel">Sign Up to Somaiya Connect</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -21,22 +21,26 @@
             <div class="modal-body">
                     <div class="form-group">
                         <label for="exampleInputEmail1">User Name</label>
-                        <input type="text" class="form-control" id="username" aria-describedby="emailHelp" name="username">
+                        <input type="text" class="form-control" id="username" aria-describedby="emailHelp" name="username" placeholder="enter your username">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">User Email</label>
-                        <input type="text" class="form-control" id="signupEmail" name="signupEmail">
+                        <label for="exampleInputEmail">User Email</label>
+                        <input type="text" class="form-control" id="useremail" name="useremail" placeholder="username@domain.com">
                     </div>
+                    <!-- <div class="form-group">
+                        <label for="loginEmail">User Email</label>
+                        <input type="text" class="form-control" id="loginEmail" aria-describedby="emailHelp" name="loginEmail" placeholder="username@domain.com">
+                    </div> -->
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="signupPassword" name="signupPassword">
+                        <input type="password" class="form-control" id="signupPassword" name="signupPassword" placeholder="enter your password">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword2">Confirm Password</label>
-                        <input type="password" class="form-control" id="signupcPassword" name="signupcPassword">
+                        <input type="password" class="form-control" id="signupcPassword" name="signupcPassword" placeholder="re-enter your password">
                     </div>
                     
-                    <button type="submit" class="btn btn-primary">SignUp</button>
+                    <button type="submit" class="btn btn-primary">Sign-Up</button>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
